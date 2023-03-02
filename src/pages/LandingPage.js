3729cd58eaa0a5019f1175151c35a66c2fca2403
Vaani <vaanipathariya@gmail.com>
2023-03-03 02:38:28 +0530

    bug: Checking linking

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,10 @@
 import "./landingpage.css"
+import { Link } from "react-router-dom";
 const LandingPage=()=>{
     return (
         <div className="landingpage">
             <div className="all-posts">
+                <Link to="/title">hello</Link>
             </div>
         </div>
     )
