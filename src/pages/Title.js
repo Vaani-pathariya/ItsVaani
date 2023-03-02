d9de8d9fe9bd80f28c374e04a88af297c0a1ea39
Vaani <vaanipathariya@gmail.com>
2023-03-03 02:36:09 +0530

    bug: Adding multiple tag functionality

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -7,7 +7,11 @@ const title=()=>{
                 <h1>Title</h1>
                 <h3>Subheading</h3>
                 <h2>Date</h2>
-                <h3 className="tags">Tags</h3>
+                <div className="tags-display">
+                    <h3 className="tags">Tags</h3>
+                    <div className="space"></div>
+                    <h3 className="tags">Tags</h3>
+                    </div>
                 <div className="content">
                     <h3>content</h3>
                     <h4 className="code">code</h4>
